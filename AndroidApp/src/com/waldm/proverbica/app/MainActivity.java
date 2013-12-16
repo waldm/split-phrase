85744483b038abbb87bb12c80e4edfdc0a55ec88
Daniel Wallis <daniel.wallis@tessella.com>
2013-12-16 10:45:09 +0000

    AndroidApp: Create app theme

--- AndroidApp/src/com/waldm/proverbica/app/MainActivity.java
+++ AndroidApp/src/com/waldm/proverbica/app/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends Activity implements OnSharedPreferenceChangeLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("");
+        setTitle("Proverbica");
         textView = (TextView) findViewById(R.id.text_box);
         imageView = (ImageView) findViewById(R.id.image);
 
