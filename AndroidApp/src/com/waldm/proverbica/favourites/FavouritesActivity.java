4a83825fda59d83b0f4f7d8d6bd499216bebae36
Daniel Wallis <daniel.wallis@tessella.com>
2013-12-22 15:39:33 +0000

    AndroidApp: Add checkbox to favourites. NOT WORKING

--- AndroidApp/src/com/waldm/proverbica/favourites/FavouritesActivity.java
+++ AndroidApp/src/com/waldm/proverbica/favourites/FavouritesActivity.java
@@ -7,9 +7,9 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.ShareActionProvider;
 import android.widget.SimpleAdapter;
@@ -21,29 +21,35 @@ import com.waldm.proverbica.R;
 public class FavouritesActivity extends ListActivity {
     private class ProverbListItem {
         private String text;
-        private boolean isFavourited;
+        private boolean isSelected;
 
         private ProverbListItem(String text) {
             this.text = text;
-            this.isFavourited = true;
         }
 
         public String getText() {
             return text;
         }
 
-        public boolean isFavourited() {
-            return isFavourited;
+        public boolean isSelected() {
+            return isSelected;
         }
 
-        public void negateFavourited() {
-            isFavourited = !isFavourited;
+        public void negateSelected() {
+            isSelected = !isSelected;
+        }
+
+        @Override
+        public String toString() {
+            return getText();
         }
     }
 
     private static final String PROVERB_KEY = "proverb_key";
-    private List<ProverbListItem> list;
+    private List<Map<String, ProverbListItem>> list;
     private ShareActionProvider shareActionProvider;
+    private SimpleAdapter adapter;
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,16 +63,11 @@ public class FavouritesActivity extends ListActivity {
         List<String> favourites = FavouritesIO.readFavourites(this);
         list = Lists.newArrayList();
         for (String favourite : favourites) {
-            list.add(new ProverbListItem(favourite));
+            list.add(ImmutableMap.of(PROVERB_KEY, new ProverbListItem(favourite)));
         }
 
-        List<Map<String, String>> adapterList = Lists.newArrayList();
-        for (ProverbListItem listItem : list) {
-            adapterList.add(ImmutableMap.of(PROVERB_KEY, listItem.getText()));
-        }
-
-        ListAdapter adapter = new SimpleAdapter(this, adapterList, R.layout.list_item_favourites,
-                new String[] { PROVERB_KEY }, new int[] { R.id.text });
+        adapter = new SimpleAdapter(this, list, R.layout.list_item_favourites, new String[] { PROVERB_KEY },
+                new int[] { R.id.text });
         setListAdapter(adapter);
     }
 
@@ -74,24 +75,29 @@ public class FavouritesActivity extends ListActivity {
     protected void onPause() {
         super.onPause();
         setListAdapter(null);
-        List<String> favourites = Lists.newArrayList();
-        for (ProverbListItem proverb : list) {
-            if (proverb.isFavourited()) {
-                favourites.add(proverb.getText());
-            }
-        }
-
-        FavouritesIO.writeFavourites(favourites, this);
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        ImageView image = (ImageView) v.findViewById(R.id.image);
-        list.get(position).negateFavourited();
-        image.setImageResource(list.get(position).isFavourited() ? android.R.drawable.btn_star_big_on
-                : android.R.drawable.btn_star_big_off);
+        list.get(position).get(PROVERB_KEY).negateSelected();
+        CheckedTextView checkedView = (CheckedTextView) v;
+        checkedView.setChecked(list.get(position).get(PROVERB_KEY).isSelected());
         updateShareIntent();
+        updateMenu();
+    }
+
+    private void updateMenu() {
+        for (Map<String, ProverbListItem> favourite : list) {
+            if (favourite.get(PROVERB_KEY).isSelected()) {
+                menu.findItem(R.id.menu_item_share).setVisible(true);
+                menu.findItem(R.id.menu_item_delete).setVisible(true);
+                return;
+            }
+        }
+
+        menu.findItem(R.id.menu_item_share).setVisible(false);
+        menu.findItem(R.id.menu_item_delete).setVisible(false);
     }
 
     @Override
@@ -100,15 +106,56 @@ public class FavouritesActivity extends ListActivity {
         shareActionProvider = (ShareActionProvider) menu.findItem(R.id.menu_item_share).getActionProvider();
 
         updateShareIntent();
+        this.menu = menu;
+        updateMenu();
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_delete:
+                deleteSelectedItems();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deleteSelectedItems() {
+        List<String> toKeep = Lists.newArrayList();
+        List<Map<String, ProverbListItem>> toDelete = Lists.newArrayList();
+        for (Map<String, ProverbListItem> favourite : list) {
+            if (favourite.get(PROVERB_KEY).isSelected()) {
+                toDelete.add(favourite);
+            } else {
+                toKeep.add(favourite.get(PROVERB_KEY).getText());
+            }
+        }
+        for (Map<String, ProverbListItem> favourite : toDelete) {
+            list.remove(favourite);
+        }
+
+        adapter.notifyDataSetChanged();
+        for (int i = 0; i < list.size(); i++) {
+            View v = adapter.getView(i, null, null);
+            CheckedTextView checkedView = (CheckedTextView) v;
+            checkedView.setChecked(false);
+        }
+
+        getListView().invalidateViews();
+
+        FavouritesIO.writeFavourites(toKeep, this);
     }
 
     private void updateShareIntent() {
         Intent shareIntent = new Intent();
         shareIntent.setAction(Intent.ACTION_SEND);
         String favourites = "";
-        for (ProverbListItem favourite : list) {
-            favourites += favourite.getText() + "\n";
+        for (Map<String, ProverbListItem> favourite : list) {
+            if (favourite.get(PROVERB_KEY).isSelected()) {
+                favourites += favourite.get(PROVERB_KEY).getText() + "\n";
+            }
         }
         shareIntent.putExtra(Intent.EXTRA_TEXT, favourites);
         shareIntent.setType("text/plain");
