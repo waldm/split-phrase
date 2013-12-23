994e7e4743c45339c60d084f8c0722609b4809b3
Daniel Wallis <daniel.wallis@tessella.com>
2013-12-23 15:34:59 +0000

    AndroidApp: Create ProverbicaImageButton and ProverbicaTextButton classes

---
+++ AndroidApp/src/com/waldm/proverbica/views/ProverbicaImageButton.java
@@ -0,0 +1,19 @@
+package com.waldm.proverbica.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class ProverbicaImageButton extends ImageView {
+    public ProverbicaImageButton(Context context) {
+        super(context);
+    }
+
+    public ProverbicaImageButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ProverbicaImageButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
---
+++ AndroidApp/src/com/waldm/proverbica/views/ProverbicaTextButton.java
@@ -0,0 +1,19 @@
+package com.waldm.proverbica.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class ProverbicaTextButton extends Button {
+    public ProverbicaTextButton(Context context) {
+        super(context);
+    }
+
+    public ProverbicaTextButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ProverbicaTextButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
