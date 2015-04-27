8c0bedf236543c4ce0c96eef1d427ef76b7a900d
Daniel Wallis <daniel.wallis@tessella.com>
2015-04-27 10:01:41 +0100

    FixFavourites: Change BaseActivity to extend AppCompatActivity

--- AndroidStudioApp/app/src/main/java/com/waldm/proverbica/BaseActivity.java
+++ AndroidStudioApp/app/src/main/java/com/waldm/proverbica/BaseActivity.java
@@ -2,9 +2,10 @@ package com.waldm.proverbica;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public abstract class BaseActivity extends ActionBarActivity{
+public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
